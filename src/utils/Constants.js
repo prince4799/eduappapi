f19ec7f42d576f00960be46aca3de2b1d802f8d7
Prince <prince.varma@kreatetechnologies.com>
2023-03-09 00:39:51 +0530

    content api api is completed

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,40 +1,45 @@
-const datetime=new Date();
+const datetime = new Date();
 
-  const error=(message='')=>{
+const error = (message = '') => {
 
-    return{
-        message:message,
-        status:false,
+    return {
+        message: message,
+        status: false,
         timestamp: datetime.toLocaleString(),
-        status:401
+        status: 401
     }
 }
 
- const success=(message,token,screen=null)=>{
+const success = (message, token, screen = null) => {
 
-    const successRes={
-        message:message,
-        status:true,
+    const successRes = {
+        message: message,
+        status: true,
         timestamp: datetime.toLocaleString(),
-        status:200 
+        status: 200
     }
-    successRes.token=token;
-    if(message.includes("Successfully logged in")){
-        successRes.screen=screen;
+    successRes.token = token;
+    if (message.includes("Successfully logged in")) {
+        successRes.screen = screen;
     }
     return successRes
 }
 
-const contentsuccess=(message,videoDetails)=>{
+const contentsuccess = (message, videoDetails) => {
 
-    const successRes={
-        message:message,
-        status:true,
+    const successRes = {
+        message: message,
+        status: true,
         timestamp: datetime.toLocaleString(),
-        status:200,
-        details:videoDetails
+        status: 200,
+        details: videoDetails
     }
-   
-    return successRes}
 
-module.exports ={error,success,contentsuccess}
+    return successRes
+}
+
+const validlength = (param) => {
+    return param.replace(/\s/g, '').length
+}
+
+module.exports = { error, success, contentsuccess, validlength }
